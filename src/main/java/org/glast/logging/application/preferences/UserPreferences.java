51bf5379d3f037b54e3987c5e77e499456b36e33
Warren Focke <focke@slac.stanford.edu>
2015-03-06 16:12:48 -0800

    Work on preferences

--- src/main/java/org/glast/logging/application/preferences/UserPreferences.java
+++ src/main/java/org/glast/logging/application/preferences/UserPreferences.java
@@ -1,11 +1,7 @@
 package org.glast.logging.application.preferences;
 
-import java.util.List;
-import java.util.Map;
 import org.srs.base.application.Application;
 import org.srs.base.application.preferences.ApplicationPreferences;
-import org.srs.web.base.utils.HistoryList;
-import org.srs.web.base.utils.HistoryMap;
 
 /**
  *
@@ -13,205 +9,35 @@ import org.srs.web.base.utils.HistoryMap;
  */
 public class UserPreferences implements ApplicationPreferences {
     
-    private static final int SIZE = 64;
-    
-    public List eventSelectionHistory = new HistoryList(SIZE);
-    public List userSelectionHistory = new HistoryList(SIZE);
-    public List programSelectionHistory = new HistoryList(SIZE);
-    public List gidSelectionHistory = new HistoryList(SIZE);
-    public List tgtSelectionHistory = new HistoryList(SIZE);
-    public List hostSelectionHistory = new HistoryList(SIZE);
-    public Map configurationMap = new HistoryMap(SIZE);
-    public String tableColumns = "Event Time,Posted Time,Level,Event,Message,Host,Target,Link";
-    
-    public String eventTimeColumnWidth = "";
-    public String postedTimeColumnWidth = "";
-    public String levelColumnWidth = "";
-    public String eventColumnWidth = "";
-    public String messageColumnWidth = "";
-    public String hostColumnWidth = "";
-    public String targetColumnWidth = "";
-    public String programColumnWidth = "";
-    public String userColumnWidth = "";
-    public String gidColumnWidth = "";
-    public String pidColumnWidth = "";
-    public String scidColumnWidth = "";
-    public String linkColumnWidth = "";
+    public String role = "spectator";
+    public String siteName = "BNL";
+    public String idAuthName = "SerialNumber";
     
 
-    public String getEventTimeColumnWidth() {
-        return eventTimeColumnWidth;
+    public String getRole() {
+        return role;
     }
         
-    public void setEventTimeColumnWidth(String colWidth) {
-        eventTimeColumnWidth = colWidth;
+    public void setRole(String role) {
+        this.role = role;
     }    
 
-    public String getPostedTimeColumnWidth() {
-        return postedTimeColumnWidth;
+    public String getSiteName() {
+        return siteName;
     }
         
-    public void setPostedTimeColumnWidth(String colWidth) {
-        postedTimeColumnWidth = colWidth;    
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;    
     }
 
-    public String getLevelColumnWidth() {
-        return levelColumnWidth;
+    public String getIdAuthName() {
+        return idAuthName;
     }
         
-    public void setLevelColumnWidth(String colWidth) {
-        levelColumnWidth = colWidth;    
+    public void setIdAuthName(String idAuthName) {
+        this.idAuthName = idAuthName;    
     }
     
-    public String getEventColumnWidth() {
-        return eventColumnWidth;
-    }
-        
-    public void setEventColumnWidth(String colWidth) {
-        eventColumnWidth = colWidth;    
-    }
-    
-    public String getMessageColumnWidth() {
-        return messageColumnWidth;
-    }
-        
-    public void setMessageColumnWidth(String colWidth) {
-        messageColumnWidth = colWidth;    
-    }
-    
-    public String getHostColumnWidth() {
-        return hostColumnWidth;
-    }
-        
-    public void setHostColumnWidth(String colWidth) {
-        hostColumnWidth = colWidth;    
-    }
-
-    public String getTargetColumnWidth() {
-        return targetColumnWidth;
-    }
-        
-    public void setTargetColumnWidth(String colWidth) {
-        targetColumnWidth = colWidth;    
-    }
-
-    public String getProgramColumnWidth() {
-        return programColumnWidth;
-    }
-        
-    public void setProgramColumnWidth(String colWidth) {
-        programColumnWidth = colWidth;    
-    }
-    
-    public String getUserColumnWidth() {
-        return userColumnWidth;
-    }
-        
-    public void setUserColumnWidth(String colWidth) {
-        userColumnWidth = colWidth;    
-    }
-    
-    public String getGidColumnWidth() {
-        return gidColumnWidth;
-    }
-        
-    public void setGidColumnWidth(String colWidth) {
-        gidColumnWidth = colWidth;    
-    }
-    
-    public String getPidColumnWidth() {
-        return pidColumnWidth;
-    }
-        
-    public void setPidColumnWidth(String colWidth) {
-        pidColumnWidth = colWidth;    
-    }
-    
-    public String getScidColumnWidth() {
-        return scidColumnWidth;
-    }
-        
-    public void setScidColumnWidth(String colWidth) {
-        scidColumnWidth = colWidth;    
-    }    
-
-    public String getLinkColumnWidth() {
-        return linkColumnWidth;
-    }
-        
-    public void setLinkColumnWidth(String colWidth) {
-        linkColumnWidth = colWidth;    
-    }    
-    
-    public String getTableColumns() {
-        return tableColumns;
-    }
-        
-    public void setTableColumns(String tableColumns) {
-        this.tableColumns = tableColumns;
-    }
-    
-    public List getEventSelection() {
-        return eventSelectionHistory;
-    }
-    
-    public void setEventSelection(List list) {
-        eventSelectionHistory = list;
-    }
-
-    public List getHostSelection() {
-        return hostSelectionHistory;
-    }
-    
-    public void setHostSelection(List list) {
-        hostSelectionHistory = list;
-    }
-    
-    public List getTgtSelection() {
-        return tgtSelectionHistory;
-    }
-    
-    public void setTgtSelection(List list) {
-        tgtSelectionHistory = list;
-    }
-
-    public List getGidSelection() {
-        return gidSelectionHistory;
-    }
-    
-    public void setGidSelection(List list) {
-        gidSelectionHistory = list;
-    }
-
-    public List getUsrSelection() {
-        return userSelectionHistory;
-    }
-    
-    public void setUsrSelection(List list) {
-        userSelectionHistory = list;
-    }
-
-    public List getProgSelection() {
-        return programSelectionHistory;
-    }
-    
-    public void setProgSelection(List list) {
-        programSelectionHistory = list;
-    }
-
-    public void addToConfiguration(String configuration, String value) {
-        configurationMap.put(configuration,value);
-    }
-    
-    public Map getConfiguration() {
-        return configurationMap;
-    }
-    
-    public void setConfiguration(Map configurationMap) {
-        this.configurationMap = configurationMap;
-    }
-    
-
     public void synchronizeWithApplication(Application app, ApplicationPreferences oldPreferences) {
     }
 
---
+++ src/main/webapp/preferences.jsp
@@ -0,0 +1,43 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql" %>
+<%@taglib prefix="preferences" uri="http://srs.slac.stanford.edu/preferences" %>
+<%@ page import="java.util.*" %>
+<%@ page isELIgnored="false" %>
+<%@ page contentType="text/html" %>
+
+<html>
+    <head>
+        <title>Preferences</title>
+    </head>
+    
+    <body>
+        <sql:query var="roleQ">
+            select id, name from PermissionGroup
+        </sql:query>
+        <sql:query var="siteQ" >
+            select id, name from Site;
+        </sql:query>
+        <sql:query var="idAuthQ" >
+            select id, name from HardwareIdentifierAuthority;
+        </sql:query>
+    <preferences:preferences name="preferences">
+        <preferences:preference name="role" title="Role: ">
+            <c:forEach var="row" items="${roleQ.rows}">
+                <preferences:value value="${row.name}"/>
+            </c:forEach>
+        </preferences:preference>
+        <preferences:preference name="siteName" title="Site: ">
+            <c:forEach var="row" items="${siteQ.rows}">
+                <preferences:value value="${row.name}"/>
+            </c:forEach>
+        </preferences:preference>
+        <preferences:preference name="idAuthName" title="Identifier Authority: ">
+            <c:forEach var="row" items="${idAuthQ.rows}">
+                <preferences:value value="${row.name}"/>
+            </c:forEach>
+        </preferences:preference>
+    </preferences:preferences>
+
+    </body>
+</html>
+
