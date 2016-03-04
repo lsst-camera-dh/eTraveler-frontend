e0cf9e714352026f16cf7fdcb9bc976c8120a22a
Warren Focke <focke@slac.stanford.edu>
2016-03-03 16:42:00 -0800

    yaml upload works

---
+++ src/main/java/eTraveler/yamlUploader.java
@@ -0,0 +1,44 @@
+package eTraveler;
+
+import java.util.Map;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import javax.servlet.http.HttpServletRequest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.lsst.camera.etraveler.backend.node.Traveler;
+
+/**
+ *
+ * @author focke
+ */
+public class yamlUploader extends SimpleTagSupport {
+    private String var;
+    private Map<String, String> parms;
+
+    @Override
+    public void doTag() throws JspException {
+        String response;
+        PageContext context = (PageContext) getJspContext();
+        HttpServletRequest req = (HttpServletRequest) context.getRequest();
+        Map<String, String> resMap = Traveler.ingest(req, parms);
+        try {
+            response = new ObjectMapper().writeValueAsString(resMap);
+        } catch (JsonProcessingException ex) {
+            throw new JspException("Error in yamlUploader tag", ex);
+        }
+        context.setAttribute(var, response);
+    }
+    
+    public void setParms(Map<String, String> parms) {
+        this.parms = parms;
+    }
+    
+    public void setVar(String var) {
+        this.var = var;
+    }
+}
