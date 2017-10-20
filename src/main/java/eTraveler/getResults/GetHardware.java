26d49a48f4de737df716b127a29a2efd1803ddd4
Joanne Bogart <jrb@slac.stanford.edu>
2017-10-20 15:13:08 -0700

    Use new cached field Hardware.locationId, starting to address LSSTTD-1189

--- src/main/java/eTraveler/getResults/GetHardware.java
+++ src/main/java/eTraveler/getResults/GetHardware.java
@@ -86,21 +86,22 @@ public class GetHardware {
 
     findHistoryRows += " group by HSH2.hardwareId) ";
 
-    String findLocationHistoryRows =
-      "(select max(HLH2.id) from HardwareLocationHistory HLH2 group by HLH2.hardwareid)"; 
+    //String findLocationHistoryRows =
+    //  "(select max(HLH2.id) from HardwareLocationHistory HLH2 group by HLH2.hardwareid)"; 
     
     String sql="select H.id as hid,lsstId,  "
       + "remarks, model, manufacturer,manufacturerId, "
       + "HS.name as status, concat(Site.name,':', Location.name) as loc, "
-      + "HSH.creationTS as statusSetTS, HLH.creationTS as locationSetTS "
+      + "HSH.creationTS as statusSetTS " //, HLH.creationTS as locationSetTS "
       + "from Hardware H "
       + "join HardwareStatusHistory HSH on H.id=HSH.hardwareId "
       + "join HardwareStatus HS on HS.id=HSH.hardwareStatusId "
-      + "join HardwareLocationHistory HLH on H.id=HLH.hardwareId "
-      + "join Location on HLH.locationId = Location.id "
+      //+ "join HardwareLocationHistory HLH on H.id=HLH.hardwareId "
+      //+ "join Location on HLH.locationId = Location.id "
+      + "join Location on H.locationId = Location.id "
       + "join Site on Site.id=Location.siteId "
       + " where HSH.id in " + findHistoryRows
-      + " and HLH.id in " + findLocationHistoryRows
+      //+ " and HLH.id in " + findLocationHistoryRows
       + " order by hid";
 
     stmt =
@@ -127,8 +128,8 @@ public class GetHardware {
       instance.put("statusSetTS",
                    GetResultsUtil.timeISO(rs.getString("statusSetTS")));
       instance.put("location", rs.getString("loc"));
-      instance.put("locationSetTS",
-                   GetResultsUtil.timeISO(rs.getString("locationSetTS")));
+      //instance.put("locationSetTS",
+      //             GetResultsUtil.timeISO(rs.getString("locationSetTS")));
       if (hardwareLabels != null) {
         instance.put("hardwareLabels", hidToLabels.get(rs.getInt("hid")));
       }
